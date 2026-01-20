89b234ffc3d2dba226daea95edd2dd07ba05b287
Okihita Sihaloho <okihita@gmail.com>
2026-01-20 13:17:43 +0700

    refactor: allow images origin and kill 3000 before pnpm dev

--- web/next.config.ts
+++ web/next.config.ts
@@ -8,6 +8,21 @@ const nextConfig: NextConfig = {
         hostname: 'asset.tix.id',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.cgv.id',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'web3.21cineplex.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.21cineplex.com',
+        pathname: '/**',
+      },
     ],
   },
 };
